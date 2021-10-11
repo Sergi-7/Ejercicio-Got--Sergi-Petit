6d7cb67fb5bf833384b6066e21dd599e35cdb15b
Sergi Petit <sergi_hak_7@hotmail.com>
2021-10-11 13:09:14 +0200

    Added shieldbearer subclass

--- gotShieldbearer.js
+++ gotShieldbearer.js
@@ -1,7 +1,24 @@
 import gotCharacter from "./gotCharacter";
 
 class gotShieldbearer extends gotCharacter {
-  constructor() {
-    super();
+  constructor(
+    name,
+    family,
+    age,
+    status,
+    series,
+    servedCharacter,
+    peloteoLevel
+  ) {
+    super(name, family, age, status, series);
+    this.name = name;
+    this.family = family;
+    this.age = age;
+    this.status = status;
+    this.servedCharacter = servedCharacter;
+    this.peloteoLevel = peloteoLevel;
+  }
+  comunicate() {
+    return "Soy un loser";
   }
 }
