4431aa2743e222d64214a452b2db2debe740fdf0
Sergi Petit <sergi_hak_7@hotmail.com>
2021-10-11 13:03:29 +0200

    Add assistant subclass

--- gotAssistant.js
+++ gotAssistant.js
@@ -1,3 +1,15 @@
 import gotCharacter from "./gotCharacter";
 
-class gotAssistant extends gotCharacter {}
+class gotAssistant extends gotCharacter {
+  constructor(name, family, age, status, series, asesedCharacter) {
+    super(name, family, age, status, series);
+    this.name = name;
+    this.family = family;
+    this.age = age;
+    this.status = status;
+    this.asesedCharacter = asesedCharacter;
+  }
+  comunicate() {
+    return "No sé por qué, pero creo que voy a morir pronto";
+  }
+}
